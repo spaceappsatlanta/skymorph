565fc05a318957ad0ec17f670999ab9ad4570c58
Zac Stewart <zgstewart@gmail.com>
2013-04-20 13:12:21 -0400

    Drop first two rows of table

--- lib/skymorph/parsers/observation_table_parser.rb
+++ lib/skymorph/parsers/observation_table_parser.rb
@@ -4,9 +4,7 @@ module Skymorph
   class ObservationTableParser
     def self.parse(raw)
       doc = Nokogiri::HTML(raw)
-      binding.pry
-      #this should get us the first table more cleanly and then select all rows
-      doc.xpath('/html/body/form/table[1]/tr')
+      doc.xpath('/html/body/form/table[1]/tr').drop(2)
     end
   end
 end
