161530a5812e68f7663e23af3314d51103cfdbdf
James Dabbs <jamesdabbs@gmail.com>
2013-04-20 15:54:12 -0400

    Use Curb to fetch requests

--- lib/skymorph/requests/request.rb
+++ lib/skymorph/requests/request.rb
@@ -1,18 +1,9 @@
+require 'curb'
+
 module SkyMorph
   class Request
     def fetch
-      uri  = URI URI.encode url
-      html = StringIO.new
-
-      @http_client.start(uri.host, uri.port) do |http|
-        request = Net::HTTP::Get.new uri.request_uri
-        http.request request do |response|
-          response.read_body do |chunk|
-            html.puts chunk
-          end
-        end
-      end
-      html.string
+      Curl::Easy.perform(url).body_str
     end
   end
 end
