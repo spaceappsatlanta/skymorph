0f6a56dc55efbd0bb9fca6dab027413278ca4430
Thomas Holmes <thomas@devminded.com>
2013-04-20 01:59:32 -0400

    Use nokogiri to parse out tile metadata

--- spec/parsers/tile_parser_spec.rb
+++ spec/parsers/tile_parser_spec.rb
@@ -7,6 +7,7 @@ describe SkyMorph::TileParser do
     let(:parse_result) { parser.parse_html(Fixtures.one_tile_html) }
 
     it { expect(parse_result).to_not eq nil }
-    it { expect(parse_result.is_a? Hash).to eq true }
+    it { expect(parse_result).to be_a_kind_of(Hash) }
+    it { expect(parse_result).to include(:_output_ql) }
   end
 end
