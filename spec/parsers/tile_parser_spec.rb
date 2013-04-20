c5555a8064e7d043be999d6b3def9b95b21716de
Justin Burris <justinbburris@gmail.com>
2013-04-20 12:08:35 -0400

    More basic parser specs

--- spec/parsers/tile_parser_spec.rb
+++ spec/parsers/tile_parser_spec.rb
@@ -5,16 +5,20 @@ describe SkyMorph::TileParser do
 
   describe "parse single tile" do
     let(:parse_result) { parser.parse_tile_result(Fixtures.one_tile_html) }
+    let(:tile) { parse_result.first }
 
     it { expect(parse_result).to_not eq nil }
     it { expect(parse_result).to be_a_kind_of(Array) }
+    it { expect(tile.keys.length).to eq 93 }
+    it { expect(tile[:output]).to eq "../tempspace/fits/skv4473613526489" }
   end
 
   describe "parse multiple tiles" do
-    let(:parse_result) { parser.parse_tile_result(Fixtures.two_tile_html)}
+    let(:parse_result) { parser.parse_tile_result(Fixtures.two_tile_html) }
 
     it { expect(parse_result).to_not eq nil }
     it { expect(parse_result).to be_a_kind_of(Array) }
     it { expect(parse_result.length).to eq 2 }
+
   end
 end
