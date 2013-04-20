ab774572ca68f3982272eca75f0ca346b5cf691f
James Dabbs <jamesdabbs@gmail.com>
2013-04-20 15:28:55 -0400

    Fix undefined method 'empty?' for URI:HTTP on 1.9.3

--- lib/skymorph/requests/request.rb
+++ lib/skymorph/requests/request.rb
@@ -5,7 +5,7 @@ module SkyMorph
       html = StringIO.new
 
       @http_client.start(uri.host, uri.port) do |http|
-        request = Net::HTTP::Get.new uri
+        request = Net::HTTP::Get.new uri.request_uri
         http.request request do |response|
           response.read_body do |chunk|
             html.puts chunk
