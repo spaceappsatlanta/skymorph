622f64d4c09be93461fa3d35aa60a31da53dcd14
James Dabbs <jamesdabbs@gmail.com>
2013-04-20 12:33:49 -0400

    Add basic error handling

--- spec/parsers/image_parser_spec.rb
+++ spec/parsers/image_parser_spec.rb
@@ -4,4 +4,8 @@ describe SkyMorph::ImageParser do
   it 'can parse image urls from html' do
     subject.parse_html(Fixtures.neat_image).should =~ /^http.*\.gif$/
   end
+
+  it 'raises a parse erorr on bad inputs' do
+    expect { subject.parse_html('garbage') }.to raise_error SkyMorph::ParseError
+  end
 end
