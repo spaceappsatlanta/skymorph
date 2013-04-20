6eb62b9d4f48b1b72e60daf4961a44e72ac3b16e
Zac Stewart <zgstewart@gmail.com>
2013-04-20 15:02:11 -0400

    Parse observation_id, triplet, time and predicted_position

--- lib/skymorph/parsers/observation_table_parser.rb
+++ lib/skymorph/parsers/observation_table_parser.rb
@@ -9,9 +9,33 @@ module Skymorph
     end
 
     def self.parse_row(row)
-      key = row.xpath('td[1]/input/@value').to_s
+      key            = row.xpath('td[1]/input/@value').text
+      observation_id = row.xpath('td[2]').text
+      triplet        = triplet_to_boolean(row.xpath('td[3]').text)
+      time           = DateTime.parse(row.xpath('td[4]').text)
+      predicted_position = {
+        right_ascension: parse_to_value_object(row.xpath('td[5]').text, RightAscension),
+        declination: parse_to_value_object(row.xpath('td[6]').text, Declination)
+      }
 
-      { key: key }
+      { key: key,
+        observation_id: observation_id,
+        triplet: triplet,
+        time: time,
+        predicted_position: predicted_position }
+    end
+
+    private
+    def self.triplet_to_boolean(triplet)
+      triplet.downcase == ?y
+    end
+
+    def self.parse_to_value_object(value, klass)
+      args = value.split(/[[:space:]]/)
+      args[0] = args[0].to_i
+      args[1] = args[1].to_i
+      args[2] = args[2].to_f
+      klass.new(*args)
     end
   end
 end
--- spec/parsers/observation_table_parser_spec.rb
+++ spec/parsers/observation_table_parser_spec.rb
@@ -17,5 +17,24 @@ describe Skymorph::ObservationTableParser do
     it 'fetches the key' do
       expect(result.first[:key]).to eq('|001204124410|51882.530787037|129.062741402712|4.64001695570385|128.337645|4.0726|20.70|-4.28|-11.08|n.a.|n.a.|n.a.|71.9154214757038|547.287989060186|y|')
     end
+
+    it 'fetches the observation_id' do
+      expect(result.first[:observation_id]).to eq('001204124410')
+    end
+
+    it 'fetches the triplet' do
+      expect(result.first[:triplet]).to be(true)
+    end
+
+    it 'fetches the time' do
+      expect(result.first[:time]).to eq(DateTime.new(2000, 12, 04, 12, 44, 20))
+    end
+
+    it 'fetch the predicted_position' do
+      expect(result.first[:predicted_position]).to eq(
+        right_ascension:  Skymorph::RightAscension.new(8, 36, 15.06),
+        declination:      Skymorph::Declination.new(4, 38, 24.1)
+      )
+    end
   end
 end
