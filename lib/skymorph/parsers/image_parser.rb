2f1eacd749fa7c273f93a083af34949c02e0a5a4
Thomas Holmes <thomas@devminded.com>
2013-04-21 10:24:59 -0400

    Refactor image parsing to return a hash

--- lib/skymorph/parsers/image_parser.rb
+++ lib/skymorph/parsers/image_parser.rb
@@ -2,10 +2,10 @@ module SkyMorph
   class ImageParser
     @@url_base = 'http://skyview.gsfc.nasa.gov'
 
-    def self.parse_html(input)
+    def self.parse(input)
       input.scan(/href='(.*?\.fits)'.*?img src='(.*?)'/mi).
-        map { |fits, src| Image.new(:path => abspath(src), :fits_path => abspath(fits)) }.
-        tap { |result| raise SkyMorph::ParseError.new "Could not parse any images" if result.empty? }
+        map { |fits, src| { :path => abspath(src), :fits_path => abspath(fits) } }.
+        tap { |result| raise SkyMorph::ParseError.new 'Could not parse any images' if result.empty? }
     end
 
     def self.abspath(path)
