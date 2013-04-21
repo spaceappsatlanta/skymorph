280ea2a563612b96c178a553d5221db682dfea6a
Zac Stewart <zgstewart@gmail.com>
2013-04-21 10:21:42 -0400

    Convert double quote strongs to single quote

--- lib/skymorph.rb
+++ lib/skymorph.rb
@@ -1,16 +1,16 @@
-require "skymorph/version"
-require "skymorph/error"
+require 'skymorph/version'
+require 'skymorph/error'
 
-require "skymorph/parsers/image_parser"
+require 'skymorph/parsers/image_parser'
 
-require "skymorph/models/image"
-require "skymorph/models/observation"
+require 'skymorph/models/image'
+require 'skymorph/models/observation'
 
-require "skymorph/requests/request"
-require "skymorph/requests/image_request"
-require "skymorph/requests/position_request"
-require "skymorph/requests/target_request"
-require "skymorph/requests/orbit_request"
+require 'skymorph/requests/request'
+require 'skymorph/requests/image_request'
+require 'skymorph/requests/position_request'
+require 'skymorph/requests/target_request'
+require 'skymorph/requests/orbit_request'
 
 require 'skymorph/parsers/observation_table_parser'
 require 'skymorph/values/right_ascension'
--- spec/requests/orbit_request_spec.rb
+++ spec/requests/orbit_request_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-REQUEST_HASH = {epoch: "epoch", ecc: "eccentricity", per: "perihelion_distance", per_date: "perihelion_distance", om: "longitutde", w: "argument", i: "incliniation", H: "abs_magnitude"}
+REQUEST_HASH = {epoch: 'epoch', ecc: 'eccentricity', per: 'perihelion_distance', per_date: 'perihelion_distance', om: 'longitutde', w: 'argument', i: 'incliniation', H: 'abs_magnitude'}
 
 describe SkyMorph::OrbitRequest do
   let(:request) { SkyMorph::OrbitRequest.new(*REQUEST_HASH.values, http_client_double('Orbit response')) }
 
-  it "generates a url" do
+  it 'generates a url' do
     expect(request.url).to eq 'http://skyview.gsfc.nasa.gov/cgi-bin/skymorph/mobssel.pl?target=&NEAT=on&OE_EPOCH=%s&OE_EC=%s&OE_QR=%s&OE_TP=%s&OE_OM=%s&OE_W=%s&OE_IN=%s&OE_H=%s' % REQUEST_HASH.values
   end
 
-  it "can make a request" do
-    expect(request.fetch).to eq "Orbit response"
+  it 'can make a request' do
+    expect(request.fetch).to eq 'Orbit response'
   end
 end
