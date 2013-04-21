16abea402ec1f88b94b126dc987d7c48697410ee
James Dabbs <jamesdabbs@gmail.com>
2013-04-20 20:48:46 -0400

    Parse multiple images

--- lib/skymorph/models/image.rb
+++ lib/skymorph/models/image.rb
@@ -4,11 +4,7 @@ module SkyMorph
   class Image
     include Virtus
 
-    attribute :gif_path, String
+    attribute :path, String
     attribute :fits_path, String
-
-    def path type
-      send :"#{type}_path"
-    end
   end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,11 +1,5 @@
 require_relative '../../lib/skymorph/models/image'
 
 describe SkyMorph::Image do
-  subject { SkyMorph::Image.new :gif_path => 'path.gif', :fits_path => 'path.fits' }
-
-  [:gif, :fits].each do |fmt|
-    it "can look up .#{fmt}s" do
-      subject.path(fmt).should =~ /\.#{fmt}$/
-    end
-  end
+  pending
 end
